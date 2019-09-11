d300082606e1f0e87f6939071e804039cef861fe
pfidr34 <pfidr34@users.noreply.github.com>
2019-09-11 10:46:49 -0500

    Clean and adjust script

--- install.sh
+++ install.sh
@@ -5,29 +5,40 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 echo "Automated Installer Program For I2C LCD Screens"
+echo ""
 echo "Installer by Ryanteck LTD. Cloned and tweaked by Matthew Timmons-Brown for The Raspberry Pi Guy YouTube tutorial"
-echo "Updating APT & Installing python-smbus, if password is asked by sudo please enter it"
+echo "Further tweaked by pfidr34 for pihole stats"
+echo ""
+echo "Updating APT & Installing python-smbus and python-requests, if password is asked by sudo please enter it"
+echo ""
 apt-get update
-apt-get install python-smbus -y
+apt-get install python-smbus python-requests -y
 echo "Should now be installed, now checking revision"
+echo ""
 revision=`python -c "import RPi.GPIO as GPIO; print GPIO.RPI_REVISION"`
 
 if [ $revision = "1" ]
 then
 echo "I2C Pins detected as 0"
+echo ""
 cp installConfigs/i2c_lib_0.py ./i2c_lib.py
 else
 echo "I2C Pins detected as 1"
+echo ""
 cp installConfigs/i2c_lib_1.py ./i2c_lib.py
 fi
 echo "I2C Library setup for this revision of Raspberry Pi, if you change revision a modification will be required to i2c_lib.py"
-echo "Now overwriting modules & blacklist. This will enable i2c Pins"
+echo ""
+echo "Now overwriting modules & blacklist. This will enable I2C pins"
+echo ""
 cp installConfigs/modules /etc/
 cp installConfigs/raspi-blacklist.conf /etc/modprobe.d/
 printf "dtparam=i2c_arm=1\n" >> /boot/config.txt
 
+echo "Install is complete. Please press any key to now reboot."
+echo ""
+echo ""
+echo ""
 
-echo "Should be now all finished. Please press any key to now reboot. After rebooting run"
-echo "'sudo python lcd.py' from this directory"
 read -n1 -s
 sudo reboot
